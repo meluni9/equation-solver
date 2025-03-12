13a6db75cb3cb70c34db11142e8aa19111de7be2
ani163 <melnika243@gmail.com>
2025-03-12 20:56:59 +0200

    Add main script to solve quadratic equations interactively

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+
+from quadratic_equation_solver import calculate_roots
+
+
+def main():
+    if len(sys.argv) == 1:
+        a = float(input("a = "))
+        b = float(input("b = "))
+        c = float(input("c = "))
+        print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+        roots = calculate_roots(a, b, c)
+
+        print_roots(roots)
+
+    elif len(sys.argv) == 2:
+        pass
+
+
+def print_roots(roots):
+    if len(roots) == 2:
+        print(f"There are 2 roots:\nx1 = {roots[0]}\nx2 = {roots[1]}")
+    elif len(roots) == 1:
+        print(f"There are 1 root:\nx1 = {roots[0]}")
+    else:
+        print("There are 0 roots")
+
+
+if __name__ == "__main__":
+    main()
